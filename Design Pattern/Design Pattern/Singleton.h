9102e33bd95da1ec595c636cf4e0ae98c1b28eb7
Coderzhangsl <zhangsl@zhangsldeMac-mini.local>
2016-08-04 10:35:58 +0800

    单例模式

---
+++ Design Pattern/Design Pattern/Singleton.h
@@ -0,0 +1,15 @@
+//
+//  Singleton.h
+//  Design Pattern
+//
+//  Created by zhangsl on 16/8/4.
+//  Copyright © 2016年 zhangsl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Singleton : NSObject
+
++ (instancetype)shareInstance;
+
+@end
---
+++ Design Pattern/Design Pattern/Singleton.m
@@ -0,0 +1,23 @@
+//
+//  Singleton.m
+//  Design Pattern
+//
+//  Created by zhangsl on 16/8/4.
+//  Copyright © 2016年 zhangsl. All rights reserved.
+//
+
+#import "Singleton.h"
+
+@implementation Singleton
+
++ (instancetype)shareInstance {
+    static Singleton *singleton = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        singleton = [[Singleton alloc] init];
+    });
+    
+    return singleton;
+}
+
+@end
